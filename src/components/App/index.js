a538e3b00126738f9eaa0893422060bc995b637b
Krystian Parma <krystian.parma@tsh.io>
2017-08-07 22:13:01 +0200

    Initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './style.scss';
+
+const App = () => (
+  <div className={styles.app}>
+    <div className={styles.app__content} />
+  </div>
+);
+
+export default App;
